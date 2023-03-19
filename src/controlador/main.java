530e39d57e272d9202f26358dafc1e2beddbbed1
Juan Pablo BAutista <jbautistaclavijo@gmail.com>
2023-03-19 16:14:45 -0500

    commit de prueba

---
+++ src/controlador/main.java
@@ -0,0 +1,17 @@
+
+package controlador;
+import visual.Menu;
+
+
+/**
+ * @author juanpbaucl514
+ */
+public class main {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+        
+    }
+    
+}
